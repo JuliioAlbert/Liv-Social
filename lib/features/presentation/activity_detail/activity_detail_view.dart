f69f52c1d90eac767957c06004660e5cef3121a5
Guillermo Jeremy De La Cruz Onton <guillermo.dlco@outlook.com>
2021-05-10 08:49:21 -0500

    Activity detail view

--- lib/features/presentation/activity_detail/activity_detail_view.dart
+++ lib/features/presentation/activity_detail/activity_detail_view.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:liv_social/core/extension/date_extension.dart';
+import 'package:liv_social/core/extension/string_extension.dart';
+import 'package:liv_social/core/localization/keys.dart';
+import 'package:liv_social/core/theme/pallete_color.dart';
 import 'package:liv_social/features/domain/entities/activity.dart';
 import 'package:liv_social/features/presentation/activity_detail/activity_detail_cubit.dart';
 
@@ -24,12 +29,230 @@ class ActivityDetailView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SafeArea(
-        child: Container(
-          color: Colors.transparent,
-          child: const Text('ActivityDetail'),
+        child: _ActivityDetailsBody(),
+      ),
+    );
+  }
+}
+
+class _ActivityDetailsBody extends StatelessWidget {
+  const _ActivityDetailsBody({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ActivityDetailCubit, ActivityDetailState>(
+        builder: (context, state) => Container(
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [
+                      Color(0xff833ab4),
+                      Color(0xfffd1d1d),
+                      Color(0xfffcb045),
+                    ],
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                    stops: [0, .35, 1]),
+              ),
+              child: Stack(
+                children: [
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: IconButton(
+                      icon: const Icon(Icons.close),
+                      onPressed: () => Navigator.of(context).pop(),
+                    ),
+                  ),
+                  const Align(
+                    alignment: Alignment.center,
+                    child: _ActivityCard(),
+                  ),
+                ],
+              ),
+            ));
+  }
+}
+
+class _ActivityCard extends StatelessWidget {
+  const _ActivityCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final bloc = context.watch<ActivityDetailCubit>();
+    final activity = bloc.activity;
+    final size = MediaQuery.of(context).size;
+    return BlocBuilder<ActivityDetailCubit, ActivityDetailState>(
+      builder: (context, state) {
+        return Container(
+          width: size.width * .7,
+          height: size.height * .7,
+          padding: const EdgeInsets.all(8),
+          alignment: Alignment.center,
+          margin: EdgeInsets.symmetric(horizontal: size.width * .08),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(25),
+            gradient: const LinearGradient(
+                colors: [
+                  Color(0xff020024),
+                  Color(0xff090979),
+                  Color(0xff00d4ff),
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                stops: [0, .65, 1]),
+            image: activity.image != null
+                ? DecorationImage(
+                    image: NetworkImage(activity.image!), fit: BoxFit.contain)
+                : null,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              _WhenField(
+                dateString: activity.expectedDate!.formatyyyyMMddHHmm(),
+              ),
+              _ActivityField(
+                fontSize: 30.0,
+                value: activity.title,
+              ),
+              _ActivityField(
+                fontSize: 20.0,
+                value: activity.subtitle,
+              ),
+              _ActivityField(
+                fontSize: 15.0,
+                value: activity.details,
+              ),
+              _LocationField(address: activity.locationPlace.address),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
+
+typedef TextCallback = Function(String text);
+
+class _ActivityField extends StatelessWidget {
+  const _ActivityField({
+    Key? key,
+    required this.value,
+    required this.fontSize,
+  }) : super(key: key);
+
+  final String value;
+
+  final double fontSize;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Text(value,
+          maxLines: 3,
+          style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w700,
+              fontSize: fontSize)),
+    );
+  }
+}
+
+class _WhenField extends StatelessWidget {
+  const _WhenField({
+    Key? key,
+    required this.dateString,
+  }) : super(key: key);
+  final String dateString;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          flex: 1,
+          child: Center(
+            child: Text(
+              '${Keys.when.localize()}:',
+              style: const TextStyle(
+                  color: Colors.white, fontWeight: FontWeight.w700),
+            ),
+          ),
         ),
+        Expanded(
+          flex: 3,
+          child: Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: Container(
+              color: Colors.transparent,
+              child: Text(
+                '$dateString',
+                style: const TextStyle(
+                    color: Colors.white, fontWeight: FontWeight.w700),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _LocationField extends StatelessWidget {
+  const _LocationField({
+    Key? key,
+    required this.address,
+  }) : super(key: key);
+  final String address;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              '${Keys.where.localize()}:',
+              style: const TextStyle(
+                  color: Colors.white, fontWeight: FontWeight.w700),
+            ),
+          ),
+          Row(
+            children: [
+              Expanded(
+                flex: 3,
+                child: Column(
+                  children: [
+                    Text(
+                      '$address',
+                      style: const TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.w700),
+                    ),
+                  ],
+                ),
+              ),
+              const Expanded(
+                flex: 1,
+                child: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: Icon(
+                    Icons.location_on,
+                    color: PalleteColor.actionButtonColor,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
